f28b3e723c0098d6d9df311de15fc8c73f775469
Crystal Broach <crysland@gmail.com>
2020-01-04 13:27:22 -0600

    installed and added propTypes

--- src/components/shared/MyNavbar/MyNavbar.js
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import PropTypes from 'prop-types';
 
 import './MyNavbar.scss';
 
 class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  }
+
   logMeOut = (e) => {
     e.preventDefault();
     firebase.auth().signOut();
@@ -16,7 +21,7 @@ class MyNavbar extends React.Component {
       <div className="MyNavbar">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container">
-          <a class="navbar-brand" href="/">Pinterest</a>
+          <a className="navbar-brand" href="/">Pinterest</a>
           {
             (authed) && <button className="nav-link btn btn-light" onClick={this.logMeOut}>Logout</button>
           }
