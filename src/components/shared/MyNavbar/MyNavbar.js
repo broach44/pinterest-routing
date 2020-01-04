925b1d96ffd8b273eebc43698bc886d24ac5d778
Crystal Broach <crysland@gmail.com>
2020-01-04 13:11:04 -0600

    navbar auth complete

---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <div className="MyNavbar">
+        <h1>I am a Navbar</h1>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;
