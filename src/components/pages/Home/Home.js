5fa3cf5de192a94977cfcf6cfabb7d1072bf3569
Crystal Broach <crysland@gmail.com>
2020-01-07 21:28:40 -0600

    delete working for boards and pins

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -12,12 +12,24 @@ class Home extends React.Component {
     boards: [],
   }
 
-  componentDidMount() {
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
       })
       .catch((errFromGetBoards) => console.error(errFromGetBoards));
+  }
+
+  componentDidMount() {
+    this.getBoards();
+  }
+
+  deleteBoard = (boardId) => {
+    boardData.deleteBoard(boardId)
+      .then(() => {
+        this.getBoards();
+      })
+      .catch((error) => console.error('err from deleteBoard', error));
   }
 
   render() {
@@ -26,7 +38,7 @@ class Home extends React.Component {
         <h1>Home Page</h1>
         <div className="boards d-flex flex-wrap justify-content-around">
           {
-            this.state.boards.map((board) => <Board key={board.id} board={board} />)
+            this.state.boards.map((board) => <Board key={board.id} board={board} deleteBoard={this.deleteBoard} />)
           }
         </div>
       </div>
