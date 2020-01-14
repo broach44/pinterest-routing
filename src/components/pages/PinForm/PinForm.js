4084d410c2b518ffd30f9ae960fee097a702b9de
Crystal Broach <crysland@gmail.com>
2020-01-13 21:29:48 -0600

    update pin complete

--- src/components/pages/PinForm/PinForm.js
+++ src/components/pages/PinForm/PinForm.js
@@ -11,6 +11,18 @@ class PinForm extends React.Component {
     pinTitle: '',
   }
 
+  componentDidMount() {
+    const { pinId } = this.props.match.params;
+    if (pinId) {
+      pinData.getSinglePin(pinId)
+        .then((request) => {
+          const pin = request.data;
+          this.setState({ pinImage: pin.imageUrl, pinTitle: pin.title });
+        })
+        .catch((err) => console.error('err from getsinglepin', err));
+    }
+  }
+
   imageChange = (e) => {
     e.preventDefault();
     this.setState({ pinImage: e.target.value });
@@ -19,6 +31,20 @@ class PinForm extends React.Component {
   pinTitleChange = (e) => {
     e.preventDefault();
     this.setState({ pinTitle: e.target.value });
+  }
+
+  editPinEvent = (e) => {
+    e.preventDefault();
+    const { boardId, pinId } = this.props.match.params;
+    const newPinObj = {
+      uid: authData.getUid(),
+      boardId,
+      imageUrl: this.state.pinImage,
+      title: this.state.pinTitle,
+    };
+    pinData.updatePin(pinId, newPinObj)
+      .then(() => this.props.history.push(`/board/${boardId}`))
+      .catch((err) => console.error('err from editPin', err));
   }
 
   savePinEvent = (e) => {
@@ -37,6 +63,7 @@ class PinForm extends React.Component {
 
   render() {
     const { pinTitle, pinImage } = this.state;
+    const { pinId } = this.props.match.params;
     return (
       <div className="PinForm">
         <h1>Create a New Pin</h1>
@@ -63,7 +90,10 @@ class PinForm extends React.Component {
               onChange={this.imageChange}
             />
           </div>
-          <button className="btn btn-secondary" onClick={this.savePinEvent}>Save Changes</button>
+          {
+            (pinId) ? <button className="btn btn-secondary" onClick={this.editPinEvent}>Edit</button>
+              : <button className="btn btn-secondary" onClick={this.savePinEvent}>Save Changes</button>
+          }
         </form>
       </div>
     );
